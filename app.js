6ff223e9e3a8befb3e0e2ffefb0975af254264da
0x-m1cro <0xm1cro@proton.me>
2023-09-12 13:58:22 +0500

    init

--- app.js
+++ app.js
@@ -1,61 +1,48 @@
-const express = require("express");
+// app.js
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', async (req, res) => {
+  let browser = null;
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  try {
+    // Launch Chrome
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+    // Create a new page
+    let page = await browser.newPage();
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    // Navigate to a website
+    await page.goto('https://example.com');
+
+    // Take a screenshot
+    let screenshot = await page.screenshot({ encoding: 'binary' });
+
+    // Send the screenshot as a response
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshot.length,
+    });
+    res.end(screenshot, 'binary');
+  } catch (error) {
+    // Handle any errors
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  } finally {
+    // Close the browser
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));

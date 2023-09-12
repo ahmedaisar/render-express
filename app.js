48344a0bc3a509dc646601a3b0556d5609e68195
0x-m1cro <0xm1cro@proton.me>
2023-09-12 17:46:26 +0500

    new

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 // app.js
 const express = require('express');
 const app = express();
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
 
-const browserFetcher = puppeteer.createBrowserFetcher();
-let revisionInfo = await browserFetcher.download("809590");
-console.log(revisionInfo)
+const exePath =
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : process.platform === "linux"
+    ? "/usr/bin/google-chrome"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
+async function getOptions(isDev) {
+  let options;
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  } else {
+    options = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    };
+  }
+  return options;
+}
+ 
 app.get('/', async (req, res) => {
+  const isDev = req.query.isDev === "false";
   let query = req.query;
   const { hotelid, checkin, checkout } = query;
 
-  if(revisionInfo?.executablePath){
+  if(revisionInfo){
     try {
-      const options = {
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security", '--font-render-hinting=none'],
-          defaultViewport: chrome.defaultViewport,
-          executablePath: revisionInfo.executablePath,
-          headless: true,
-          ignoreHTTPSErrors: true,
-      };
+      const options = await getOptions(isDev);
       const browser = await puppeteer.launch(options);
-  
       const page = await browser.newPage();
   
       await page.goto(
         `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
         {
-          waitUntil: "networkidle2",
+          waitUntil: "networkidle0",
         }
       );
       // let html = await page.evaluate(() => {
@@ -36,11 +51,14 @@ app.get('/', async (req, res) => {
       // });
       let body = await page.waitForSelector('body');
       let json = await body?.evaluate(el => el.textContent);
-      await page.waitForTimeout(1000);    
       await browser.close();
       res.status(200).json(json);
     } catch (error) {
       console.log(error);
+      res.statusCode = 500;
+      res.json({
+        body: "Sorry, Something went wrong!",
+      });
     }
     
   }

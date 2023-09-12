9dae44c2e408b8932753c09194a1cc34ea458bd1
0x-m1cro <0xm1cro@proton.me>
2023-09-12 18:02:25 +0500

    new#2

--- app.js
+++ app.js
@@ -3,39 +3,21 @@ const express = require('express');
 const app = express();
 const puppeteer = require("puppeteer-core");
 const chrome = require("chrome-aws-lambda");
-
-const exePath =
-  process.platform === "win32"
-    ? "C:/Program Files/Google/Chrome/Application/chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-async function getOptions(isDev) {
-  let options;
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
-      headless: false,
-    };
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    };
-  }
-  return options;
-}
  
-app.get('/', async (req, res) => {
-  const isDev = false;
+app.get('/', async (req, res) => { res.send({
+  body: "Hello!",
+});  })
+
+app.get('/hotel', async (req, res) => {
   let query = req.query;
   const { hotelid, checkin, checkout } = query;
 
   try {
-      const options = await getOptions(isDev);
+      const options = {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      };
       const browser = await puppeteer.launch(options);
       const page = await browser.newPage();
   
@@ -50,8 +32,8 @@ app.get('/', async (req, res) => {
       // });
       let body = await page.waitForSelector('body');
       let json = await body?.evaluate(el => el.textContent);
-      await browser.close();
       res.status(200).json(json);
+      await browser.close();      
     } catch (error) {
       console.log(error);
       res.statusCode = 500;

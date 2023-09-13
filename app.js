08a7874a9c7139e256164839eb083e7fd0349b44
0x-m1cro <0xm1cro@proton.me>
2023-09-13 12:14:54 +0500

    working-prod-push

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // app.js
+const fs = require('fs')
 const express = require('express');
 const app = express();
-//const puppeteer = require("puppeteer-core");
 const chrome = require("chrome-aws-lambda");
+const path = require("path")
+const NodeCache = require('node-cache');
+const compression = require('compression');
+app.use(compression()); 
+app.use(express.json());
  
+const cache = new NodeCache(); 
+
 app.get('/', async (req, res) => { res.send({
   body: "Hello!",
 });  })
 
+//Anextour api routes
 app.get('/hotels', async (req, res) => { 
-
+  const cacheKey = 'anex';
+  let cachedData = cache.get(cacheKey);
   const api = "https://api.anextour.com/search/Hotels?SEARCH_MODE=b2c&SEARCH_TYPE=PACKET_ONLY_HOTELS&lang=&state=maldives&townFrom=1"
 
   try {
@@ -33,7 +42,11 @@ app.get('/hotels', async (req, res) => {
     let body = await page.waitForSelector('body');
     let json = await body?.evaluate(el => el.textContent);
     await browser.close();   
-    res.status(200).json(json);       
+    if (!cachedData) {
+      cache.set(cacheKey, json);
+      cachedData = json;    
+     }
+    res.status(200).json(cachedData);       
   } catch (error) {
     console.log(error);
     await browser.close();   
@@ -84,9 +97,12 @@ app.get('/hotel', async (req, res) => {
 
 })
 
+//Hotelscan api routes
 app.get('/scan', async (req, res) => {
   let query = req.query;
   const { hotelid, checkin, checkout } = query;
+  // const cacheKey = hotelid;
+  // let cachedData = cache.get(cacheKey);
 
   try {
       const options = {
@@ -109,7 +125,7 @@ app.get('/scan', async (req, res) => {
       let body = await page.waitForSelector('body');
       let json = await body?.evaluate(el => el.textContent);
       await browser.close();   
-      res.status(200).json(json);       
+      res.status(200).json(json);           
     } catch (error) {
       console.log(error);
       await browser.close();   
@@ -120,6 +136,31 @@ app.get('/scan', async (req, res) => {
     }
     
   });
+
+app.get('/maldives', async (req, res) => {  
+  const cacheKey = 'maldives';
+  let cachedData = cache.get(cacheKey);
+  const readFile = (path, opts = 'utf8') =>
+    new Promise((resolve, reject) => {
+      fs.readFile(path, opts, (err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      })
+  })
+  try {
+    const jsonDirectory = path.join(process.cwd(), "json");
+    const jsondata = await readFile(jsonDirectory + "/data.json");    
+    if (!cachedData) {
+      cache.set(cacheKey, jsondata);
+      cachedData = jsondata;    
+     }
+     res.status(200).json(cachedData);
+  } catch (error) {
+    console.log(error)
+  }
+  
+  
+})
 
 // Start the server
 const PORT = process.env.PORT || 3000;
